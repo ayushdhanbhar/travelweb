113fd9f18124581dff3beee413dfe44a8d1c1366
Ayush Dhanbhar <aayushdhanbhar@gmail.com>
2024-12-31 14:06:46 +0530

    initial commit

--- src/components/image-gallery/galleryImages.js
+++ src/components/image-gallery/galleryImages.js
@@ -1,4 +1,3 @@
-import galleryImg01 from '../../assets/images/gallery-01.jpg'
 import galleryImg02 from '../../assets/images/gallery-02.jpg'
 import galleryImg03 from '../../assets/images/gallery-03.jpg'
 import galleryImg04 from '../../assets/images/gallery-04.jpg'
@@ -12,27 +11,43 @@ import galleryImg12 from '../../assets/images/gallery-12.jpg'
 import galleryImg13 from '../../assets/images/gallery-13.jpg'
 import galleryImg15 from '../../assets/images/gallery-15.jpg'
 import galleryImg16 from '../../assets/images/gallery-16.jpg'
-import galleryImg17 from '../../assets/images/gallery-17.jpg'
 import galleryImg18 from '../../assets/images/gallery-18.jpg'
 import galleryImg19 from '../../assets/images/gallery-19.jpg'
+import galleryImg20 from '../../assets/images/gallery-20.jpg'
+import galleryImg21 from '../../assets/images/gallery-21.jpg'
+import galleryImg22 from '../../assets/images/gallery-22.jpg'
+import galleryImg23 from '../../assets/images/gallery-23.jpg'
+import galleryImg24 from '../../assets/images/gallery-24.jpg'
+import galleryImg25 from '../../assets/images/gallery-25.jpg'
+import galleryImg26 from '../../assets/images/gallery-26.jpg'
+import galleryImg27 from '../../assets/images/gallery-27.jpg'
+import galleryImg28 from '../../assets/images/gallery-28.jpg'
+
 const galleryImages =[
-    galleryImg01,
+
     galleryImg07,
     galleryImg02,
     galleryImg06,
     galleryImg03,
-    galleryImg04,
+    galleryImg20,
     galleryImg05,
-    galleryImg08,
+    galleryImg27,
     galleryImg09,
     galleryImg11,
     galleryImg12,
     galleryImg19,
+    galleryImg28,
     galleryImg16,
-    galleryImg17,
+    galleryImg26,
     galleryImg18,
     galleryImg13,
-    galleryImg15
+    galleryImg23,
+    galleryImg04,
+    galleryImg24,
+    galleryImg22,
+    galleryImg21,
+    galleryImg25,
+    galleryImg08
 ];
 
 export default galleryImages;
